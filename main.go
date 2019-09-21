62e79f6ceff5f9ae6ed5180b93953f52c9c955c6
atsedong <alexandra2007.tse@gmail.com>
2019-09-21 14:11:28 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
+package main
 
+import "fmt"
+
+type car interface {
+	Name() string
+}
+
+type bmw struct {
+	ID           int
+	Colour       string
+	Year         string
+	Manufacturer string
+	Country      string
+}
+
+type nissan struct {
+	ID           int
+	Colour       string
+	Year         string
+	Manufacturer string
+	Country      string
+	Owner        string
+}
+
+func (b bmw) Name() string {
+	return "BMW"
+}
+
+func (n nissan) Name() string {
+	return "Nissan"
+}
+
+func do(i car) {
+	switch v := i.(type) {
+	case bmw:
+		fmt.Printf("This is BMW, the colour is %v\n", v.Colour)
+	case nissan:
+		fmt.Printf("This is Nissan, the colour is %v and the owner of this Nissan is %v\n", v.Colour, v.Owner)
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
+}
